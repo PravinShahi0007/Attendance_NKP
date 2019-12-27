673b841a2ea26501dc37f4bf63f6c628e3eabdf0
anand1606 <anand.j.acharya@gmail.com>
2019-12-27 15:36:51 +0530

    27-12-2019 : MastShiftScheduleHistory

--- Attendance/Forms/frmUploadShiftSchedule.cs
+++ Attendance/Forms/frmUploadShiftSchedule.cs
@@ -287,6 +287,25 @@ namespace Attendance.Forms
                             }
                             else
                             {
+
+                                sql = "insert into MastShiftScheduleHistory ( [YearMt],[EmpUnqID], " +
+                                   " [D01],[D02],[D03],[D04],[D05],[D06],[D07],[D08],[D09],[D10]," +
+                                   " [D11],[D12],[D13],[D14],[D15],[D16],[D17],[D18],[D19],[D20],[D21], " +
+                                   " [D22],[D23],[D24],[D25],[D26],[D27],[D28],[D29],[D30],[D31],[ADDID], " +
+                                   " [ADDDT],[UPDID],[UPDDT]) " +
+                                   " Select  " +
+                                   " [YearMt],[EmpUnqID]," +
+                                   " [D01],[D02],[D03],[D04],[D05],[D06],[D07],[D08],[D09],[D10]," +
+                                   " [D11],[D12],[D13],[D14],[D15],[D16],[D17],[D18],[D19],[D20],[D21], " +
+                                   " [D22],[D23],[D24],[D25],[D26],[D27],[D28],[D29],[D30],[D31],[ADDID]," +
+                                   " [ADDDT],[UPDID],[UPDDT] " +
+                                   " where YearMt = '" + tYearMt + "' " +
+                                   " and EmpUnqID = '" + tEmpUnqID + "' ";
+
+                                cmd = new SqlCommand(sql, cn, trn);
+                                cmd.ExecuteNonQuery();
+                                
+                                
                                 sql = "Update MastShiftSchedule Set UpdDt = GetDate() , UpdID = '" + Utils.User.GUserID + "' Where YearMt = '" + tYearMt + "' And EmpUnqID ='" + tEmpUnqID + "'";
                                 cmd = new SqlCommand(sql, cn, trn);
                                 cmd.ExecuteNonQuery();
