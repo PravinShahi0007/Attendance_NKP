0570974e7658eca86170e6d5c3f092093d0242b4
anand1606 <anand.j.acharya@gmail.com>
2019-12-31 11:05:05 +0530

    2019-12-31 : bug fixed shift schedule upload

--- Attendance/Forms/frmUploadShiftSchedule.cs
+++ Attendance/Forms/frmUploadShiftSchedule.cs
@@ -298,7 +298,7 @@ namespace Attendance.Forms
                                    " [D01],[D02],[D03],[D04],[D05],[D06],[D07],[D08],[D09],[D10]," +
                                    " [D11],[D12],[D13],[D14],[D15],[D16],[D17],[D18],[D19],[D20],[D21], " +
                                    " [D22],[D23],[D24],[D25],[D26],[D27],[D28],[D29],[D30],[D31],[ADDID]," +
-                                   " [ADDDT],[UPDID],[UPDDT] " +
+                                   " [ADDDT],[UPDID],[UPDDT] from MastShiftSchedule " +
                                    " where YearMt = '" + tYearMt + "' " +
                                    " and EmpUnqID = '" + tEmpUnqID + "' ";
 
